d0bb42f605a34343d1b88b5e068c8962daea0ccd
Rory May <rm@nuelo.co>
2026-06-11 12:28:04 +0000

    clean up

--- admin/check-about.mjs
+++
@@ -1,7 +0,0 @@
-import { db } from './src/lib/db/index.ts'
-import { siteSections } from './src/lib/db/schema.ts'
-import { eq } from 'drizzle-orm'
-
-const rows = await db.select().from(siteSections).where(eq(siteSections.page, 'about'))
-console.log(JSON.stringify(rows, null, 2))
-process.exit(0)
