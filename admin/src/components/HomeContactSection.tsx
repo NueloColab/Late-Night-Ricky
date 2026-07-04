c1fbc9d3501805d54c149f1d3f9f653bd59ab54f
Rory May <rm@nuelo.co>
2026-07-04 08:46:18 +0000

    fix: move contact section slightly more to the right

--- admin/src/components/HomeContactSection.tsx
+++ admin/src/components/HomeContactSection.tsx
@@ -96,7 +96,7 @@ export default function HomeContactSection() {
       </div>
 
       {/* Contact content on the left */}
-      <div className="relative z-10 flex items-center min-h-[calc(100dvh-70px)] px-8 md:px-14 py-20">
+      <div className="relative z-10 flex items-center min-h-[calc(100dvh-70px)] px-12 md:px-20 py-20">
         <div className="w-full max-w-[600px]">
           {!expanded ? (
             <div className="text-left">
